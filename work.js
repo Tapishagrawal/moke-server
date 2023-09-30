b942ed7c30568ac46b797738f20600864262d6d1
Tapishagrawal <tapishg4@gmail.com>
2023-09-30 23:13:50 +0530

    all product added

--- work.js
+++ work.js
@@ -1,8 +1,5 @@
 const fs = require('fs');
 
-// Array of possible brand names
-const sizes = [["S","M","L","XL","XXL"],["S","M","XL"],["L","XL","XXL"],["S","M","L","XXL"],["S","L","XL","XXL"],["S","M","L","XL"],["S","M","XL","XXL"]];
-
 // Load the JSON data from the file
 fs.readFile('cloth_db.json', 'utf8', (err, data) => {
     if (err) {
@@ -13,20 +10,31 @@ fs.readFile('cloth_db.json', 'utf8', (err, data) => {
     // Parse the JSON data
     const jsonData = JSON.parse(data);
 
-    // Add the "brand" field with a random brand name to each object
-    jsonData.womenClothes.forEach(item => {
-        item.sizes = sizes[Math.floor(Math.random() * sizes.length)];
-    });
+    // Merge "menClothes" and "womenClothes" arrays into a new array
+    const mergedClothes = [...jsonData.menClothes, ...jsonData.womenClothes];
+
+    // Randomize the order of objects in the merged array
+    for (let i = mergedClothes.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [mergedClothes[i], mergedClothes[j]] = [mergedClothes[j], mergedClothes[i]];
+    }
+
+    // Convert the merged and shuffled data back to JSON format
+    jsonData.mergedClothes = mergedClothes;
+
+    // Remove the separate "menClothes" and "womenClothes" arrays if needed
+    delete jsonData.menClothes;
+    delete jsonData.womenClothes;
 
     // Convert the modified data back to JSON format
     const updatedData = JSON.stringify(jsonData, null, 2);
 
     // Write the updated data back to the file
-    fs.writeFile('cloth_db.json', updatedData, 'utf8', err => {
+    fs.writeFile('cloth_db.json', updatedData, 'utf8', (err) => {
         if (err) {
             console.error(err);
             return;
         }
-        console.log('Random sizes added to all objects.');
+        console.log('Merged and shuffled clothes data.');
     });
 });
