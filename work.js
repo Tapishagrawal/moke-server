6b4e09d7006f2f7e250480d83b7716c268553100
Tapishagrawal <tapishg4@gmail.com>
2023-10-01 00:22:10 +0530

    add name property in data

--- work.js
+++ work.js
@@ -1,40 +1,40 @@
-const fs = require('fs');
+// const fs = require('fs');
 
-// Load the JSON data from the file
-fs.readFile('cloth_db.json', 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
+// // Load the JSON data from the file
+// fs.readFile('cloth_db.json', 'utf8', (err, data) => {
+//     if (err) {
+//         console.error(err);
+//         return;
+//     }
 
-    // Parse the JSON data
-    const jsonData = JSON.parse(data);
+//     // Parse the JSON data
+//     const jsonData = JSON.parse(data);
 
-    // Merge "menClothes" and "womenClothes" arrays into a new array
-    const mergedClothes = [...jsonData.menClothes, ...jsonData.womenClothes];
+//     // Merge "menClothes" and "womenClothes" arrays into a new array
+//     const mergedClothes = [...jsonData.menClothes, ...jsonData.womenClothes];
 
-    // Randomize the order of objects in the merged array
-    for (let i = mergedClothes.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [mergedClothes[i], mergedClothes[j]] = [mergedClothes[j], mergedClothes[i]];
-    }
+//     // Randomize the order of objects in the merged array
+//     for (let i = mergedClothes.length - 1; i > 0; i--) {
+//         const j = Math.floor(Math.random() * (i + 1));
+//         [mergedClothes[i], mergedClothes[j]] = [mergedClothes[j], mergedClothes[i]];
+//     }
 
-    // Convert the merged and shuffled data back to JSON format
-    jsonData.mergedClothes = mergedClothes;
+//     // Convert the merged and shuffled data back to JSON format
+//     jsonData.mergedClothes = mergedClothes;
 
-    // Remove the separate "menClothes" and "womenClothes" arrays if needed
-    delete jsonData.menClothes;
-    delete jsonData.womenClothes;
+//     // Remove the separate "menClothes" and "womenClothes" arrays if needed
+//     delete jsonData.menClothes;
+//     delete jsonData.womenClothes;
 
-    // Convert the modified data back to JSON format
-    const updatedData = JSON.stringify(jsonData, null, 2);
+//     // Convert the modified data back to JSON format
+//     const updatedData = JSON.stringify(jsonData, null, 2);
 
-    // Write the updated data back to the file
-    fs.writeFile('cloth_db.json', updatedData, 'utf8', (err) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        console.log('Merged and shuffled clothes data.');
-    });
-});
+//     // Write the updated data back to the file
+//     fs.writeFile('cloth_db.json', updatedData, 'utf8', (err) => {
+//         if (err) {
+//             console.error(err);
+//             return;
+//         }
+//         console.log('Merged and shuffled clothes data.');
+//     });
+// });
